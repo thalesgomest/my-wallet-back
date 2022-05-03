66a8f51cfc589b0593b78d954d3d5e5cecbf77d2
Thales Gomes Targino <thalestargino@gmail.com>
2022-05-03 15:57:29 -0300

    fix: wakatime config

--- src/app.js
+++ src/app.js
@@ -8,3 +8,21 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db = null;
+
+mongoClient
+    .connect()
+    .then(() => {
+        db = mongoClient.db('my-wallet');
+        console.log(chalk.bold.red('Connected to MongoDB'));
+    })
+    .catch((err) => {
+        console.log(chalk.bold.red('Error connecting to MongoDB', err));
+    });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(chalk.bold.green(`Server running on port ${port}`));
+});
